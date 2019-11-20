4ce1a2f252391eb34e7f42d49e966683a0cc2746
tts2k <tranthaison2000@gmail.com>
2019-11-20 13:23:29 -0500

    initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,71 @@
+var comicId = 0;
+var latest = 0;
+let controllerDiv = document.getElementById('controller');
+
+function init(currentId, latestId) {
+    comicId = parseInt(currentId);
+    latest = parseInt(latestId);
+    createBtnPrev();
+    createBtnRand();
+    createBtnNext();
+}
+
+function createBtn(value, func, id) {
+    var btn = document.createElement('input');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('value', value);
+    btn.setAttribute('onclick', func);
+    btn.setAttribute('id', id);
+
+    return btn;
+}
+
+// Switching page
+function pageSwitch(mode) {
+    var param = '';
+    switch (mode) {
+        case 'next':
+            param = (comicId + 1).toString();
+            break;
+        case 'prev':
+            param = (comicId - 1).toString();
+            break;
+        case 'rand':
+            param = 'random';
+            break;
+        default:
+            return false;
+    }
+
+    window.location.href = '/' + param;
+}
+
+// Previous comic button
+function createBtnPrev() {
+    const id = 'prev'; // Button id
+    var btn;
+    if (comicId - 1 > 0)
+        btn = createBtn('<< Prev', `pageSwitch('${id}')`, id);
+    else
+        btn = createBtn('<< Prev', `pageSwitch('')`, id);
+    controllerDiv.appendChild(btn);
+}
+// Next comic button
+function createBtnNext() {
+    const id = 'next';
+    var btn;
+    if (comicId + 1 > latest)
+        btn = createBtn('Next >>', `pageSwitch('')`, id);
+    else 
+        btn = createBtn('Next >>', `pageSwitch('next')`, id);
+
+    controllerDiv.appendChild(btn);
+}
+// Random comic button
+function createBtnRand() {
+    const id = 'rand';
+    var btn = createBtn('Random', `pageSwitch('${id}')`, id);
+    controllerDiv.appendChild(btn);
+}
+
+// Last comic button
---
+++ routes/index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+/* GET JSON object */
+async function getJson(url, params = {}) {
+
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+  let headers = {
+    "Accept" : "application/json",
+    "Content-Type" : "application/json"
+  }
+
+  let response = await fetch(url, {
+    method: "GET",
+    headers: headers
+  });
+
+  if (response.status === 404) {
+    res.sendStatus(404);
+    return;
+  }
+
+  return await response.json();
+}
+
+/* Get latest compic strip */
+async function getLatestComic() {
+  let url = new URL('https://xkcd.com/info.0.json');
+  return await getJson(url);
+}
+
+/* GET home page. */
+router.get('/', async function(req, res) {
+  let data = await getLatestComic();
+
+  res.render('index', {
+    title: 'xkcd #' + data.num,
+    comTitle: data.title,
+    id: data.num,
+    latestId: data.num,
+    date: `${data.month}/${data.day}`,
+    imgLink: data.img
+  });
+});
+
+// get comic json from id
+router.get('/:id([0-9]+)', async function(req, res) {
+  const id = parseInt(req.params.id);
+  const url = new URL('https://xkcd.com/' + id + '/info.0.json');
+
+  let data = await getJson(url);
+  var transcript = '';
+  if (data.transcript === "")
+    transcript = "There's no official transcript for this comic strip.";
+  else
+    transcript = data.transcript.replace(/(?:\r\n|\r|\n)/g, ' <br/> ');
+  let latest = await getLatestComic();
+
+  res.render('index', {
+    title: 'xkcd #' + data.num,
+    comTitle: data.title,
+    id: data.num,
+    latestId: latest.num,
+    date: `${data.month}/${data.day}/${data.year}`,
+    imgLink: data.img,
+    transcript: transcript
+  });
+});
+
+// get a random comic trip
+router.get('/random', async function(req, res) {
+  let data = await getLatestComic();
+  let latestId = parseInt(data.num);
+  const id = parseInt(Math.random() * latestId) + 1;
+  res.redirect('/' + id);
+});
+
+module.exports = router;
